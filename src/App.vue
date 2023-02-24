296f7091ec4d06998f93e3db6482d8339eb03256
OlimUmarov <olimumarov2017@gmail.com>
2023-02-24 20:23:12 +0500

    Slidebar

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,33 @@
 <template>
 
     <div class="app">
-        <Sidebar/>
+        <SideBar/>
         <router-view/>
         <router-link to="/about"/>
     </div>
 
 </template>
 
-<script setup>
-    import Sidebar from './components/SideBar.vue'
+<script>
+    import SideBar from "./components/Sidebar.vue";
+    export default {
+        components: {SideBar}
+    }
+
 </script>
 
 <style lang="scss" scoped>
 
-    :root{
-        --primary: #4ade80;
-        --primary-alt: #22c55e;
-        --grey: #64748b;
-        --dark: #1e293b;
-        --light: #f1f5f9;
-        --sidebar-width: 300px
-    }
-    *{
-        margin: 0;
-        padding: 0;
-        min-width: 100%;
-        min-height: 100%;
-        box-sizing: border-box;
-        font-family: 'Fira Sans', sans-serif;
-    }
-    body{
-        background: var(--light);
-    }
-    button{
-        cursor: pointer;
-        appearance: none;
-        border: none;
-        outline: none;
-        background: none;
-    }
     .app{
         display: flex;
 
         main{
             flex: 1 1 0;
+            padding: 2rem;
+
+            @media (max-width: 768px) {
+                padding: 6rem;
+            }
         }
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,5 @@
 
 <style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Sofia+Sans:ital,wght@1,100;1,300&display=swap');
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Fira Sans', sans-serif;
-    }
-    html{
-        font-family: 'Fira Sans', sans-serif;
-    }
 
 </style>
